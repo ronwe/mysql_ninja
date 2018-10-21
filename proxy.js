f2f1c917f3c7e90730d2b3888c86d395a1963893
Administrator <admin@example.com>
2018-10-21 11:43:52 +0800

    cache query

--- proxy.js
+++ proxy.js
@@ -4,39 +4,26 @@ let Net = require('net')
 	,Util = require('util')
 
 let Analytic = require('./lib/select_analytic.js')
+let Cache = require('./lib/select_cache.js') 
 
 let HOST= '127.0.0.1' 
 
 let program = {
-	proxyport : 13306,
+	proxyport : 3360,
 	hostname : '172.24.0.161',
 	port : 3360
 }
 
 let PORT = program.proxyport
 
-/*
-Util.inherits(Connection, Events.EventEmitter);
-var PacketParser = require('../node_modules/mysql2/lib/packet_parser.js');
-packetParser = new PacketParser(function(p) {
-	//handlePacket(p);
-	//console.log('parsed',p.sequenceId ,p.buffer)
-	console.log('parsed',p)
-});
-packetParser2 = new PacketParser(function(p) {
-	//handlePacket(p);
-	//console.log('parsed',p.sequenceId ,p.buffer)
-	console.log('parsed2',p)
-});
-*/
 
-let Cache = require('./lib/select_cache.js') 
 
 Net.createServer(function(sock) {
 	let _sequence = [] 
 
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
     let client = new Net.Socket()
+	let _reponse_stack  = []  
     console.log("hostname is "+program.hostname + ':' +program.port);
     client.connect(program.port||3306, program.hostname, function() {
         console.log('Connected')
@@ -53,13 +40,33 @@ Net.createServer(function(sock) {
     })
 
     client.on('data', function(data) {
-		//console.log('response: ',  data);
-		///packetParser2.execute(data)
-		
+		/*
+		*第一个字节值    后续字节数  长度值说明
+		*0-250            0   第一个字节值即为数据的真实长度
+		*  251            0   空数据，数据的真实长度为零
+		*  252            2   后续额外2个字节标识了数据的真实长度
+		*  253            3   后续额外3个字节标识了数据的真实长度
+		*  254            8   后续额外8个字节标识了数据的真实长度
+		* ??? 现在是看最后一位是0x00 则结束
+		*/
+		//console.log('response: ',  data.readUInt8(0),data.length, data.slice(-5));
+		//http://mysql.taobao.org/monthly/2018/04/05/	
+		//Packet.prototype.isEOF = function() {
+		//	  return this.buffer[this.offset] == 0xfe && this.length() < 13;
+		//};
         sock.write(data)
-		let _query = _sequence.shift()
-		if (_query ){
-			Analytic.setCache(_query , data)
+		//https://jan.kneschke.de/projects/mysql/mysql-protocol/
+		//会有多次response ,得合并data？
+		_reponse_stack.push(data)
+		let last = data.readUInt8(Buffer.byteLength(data) -1)
+		//console.log('response: ',  data.readUInt8(0),data.length, data.slice(10),'|||',data.slice(-5),last)
+		if (last === 0x00){
+			let _query = _sequence.shift()
+			if (_query ){
+				Analytic.setCache(_query , _reponse_stack)
+			}
+			_reponse_stack = []
+		}else{
 		}
     })
     sock.client = client
@@ -67,7 +74,9 @@ Net.createServer(function(sock) {
     sock.on('data', function(data) {
 		//https://jin-yang.github.io/post/mysql-protocol.html
 		//https://dev.mysql.com/doc/dev/mysql-server/8.0.0/page_protocol_basic_packets.html
+		//_reponse_stack = []
 		let _detect = data.readUInt8(4)
+		//console.log('on data' ,data.toString())
 		if (_detect === 0x03){
 			let _sql = data.slice(5).toString() 
 			//console.log('sql' ,_sql)
@@ -79,8 +88,11 @@ Net.createServer(function(sock) {
 					let _query = Analytic.wrap(_sql)
 					let _cache = Cache.get(_query)
 					if (_cache){
-						console.log('from cache\n')
-						sock.write( _cache  )
+						console.log('from cache\n', _query,_cache.length  )
+						//sock.write( _cache  )
+						_cache.forEach(c => {
+							sock.write( c  )
+						})
 						return
 					
 					}else if (Analytic.isCacheAble(_query)) {
