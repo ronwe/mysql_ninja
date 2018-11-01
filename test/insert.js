7241722afbdcb956b23d8ff1b8094a2b8a658d97
Administrator <admin@example.com>
2018-11-01 14:39:50 +0800

    support replace into

--- test/insert.js
+++ test/insert.js
@@ -8,6 +8,7 @@ sql.push(`insert into dbone.u_table  (flda,fldb,fldc) values (124,"a(b)c","789")
 sql.push(`insert into dbone.u_table  (flda,fldb,fldc) values (124,"a(b)c",789) ,(456,"efg",0)`)
 
 sql.push(`insert into dbone.u_table  (flda,fldb,fldc) values (124,'     a(b)c"',789) ,(456,"ef'g",0)`)
+sql.push(`replace into tablename (f1, f2, f3) values(vf1, vf2, vf3),(vvf1, vvf2, vvf3)`)
 let _sql = sql.pop()
 console.log('\x1b[0m')
 var tables = Parser.getTableNames(_sql,'insert','dbone')
